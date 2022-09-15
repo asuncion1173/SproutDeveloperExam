76e1cd16c91c647c98fc8fbfea8b14a1d2fa5ff1
Jacob Asuncion <asuncion1173@gmail.com>
2022-09-15 16:00:25 +0800

    update on navigation bar. application of logos, fonts, and colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import React from "react";
 import '../index.css';
+import logo from '../Sprout Logo.JPG'
 function Header() {
     return(
         <nav className="nav-bar">
             <ul className="logo">
-                <li>Home</li>
+                <li><img src={logo} alt="Sprout Logo"/></li>
             </ul>
             <ul className="options">
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>   
-                <li><i class="fa fa-search" aria-hidden="true"></i>Home</li>
+                <li>My Request<i class="fa fa-sort-desc color-white" aria-hidden="true"></i></li>
+                <li>My Team<i class="fa fa-sort-desc color-white" aria-hidden="true"></i></li>
+                <li>Company<i class="fa fa-sort-desc color-white" aria-hidden="true"></i></li>
+                <li>Administration Tools<i class="fa fa-sort-desc color-white" aria-hidden="true"></i></li>
+                <li>Maintenance<i class="fa fa-sort-desc color-white" aria-hidden="true"></i></li>   
+                <li><textarea class="employee-search" placeholder="employee search"></textarea><i class="fa fa-search color-white" aria-hidden="true"></i></li>
+                <li><i class="fa fa-sort-desc color-white" aria-hidden="true"></i></li>       
+                <li><i class="fa fa-sort-desc color-white" aria-hidden="true"></i></li>
+                <li>Admin<i class="fa fa-sort-desc color-white" aria-hidden="true"></i></li>
             </ul>
         </nav>
     )
