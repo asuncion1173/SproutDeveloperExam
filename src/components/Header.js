5f730ddc2adb7ec6f164682f0a210b8042883d76
Jacob Asuncion <asuncion1173@gmail.com>
2022-09-15 14:53:20 +0800

    update on navigation bar

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import '../index.css';
+function Header() {
+    return(
+        <nav className="nav-bar">
+            <ul className="logo">
+                <li>Home</li>
+            </ul>
+            <ul className="options">
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>   
+                <li><i class="fa fa-search" aria-hidden="true"></i>Home</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
