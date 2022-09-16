e20cb1ba0c1db7688fe0e9ec1476d88b7ba23aa7
Jacob Asuncion <asuncion1173@gmail.com>
2022-09-16 21:35:12 +0800

    Tentative Final Commit for Sprout-Developer-Exam. Adjusted table footer contents and table contents. Adjusted margins aswell.

--- src/components/Table.js
+++ src/components/Table.js
@@ -24,7 +24,7 @@ function Table() {
                     <th>TITLE</th>
                     <th>MESSAGE</th>
                     <th>SENT BY</th>
-                    <th>SENT THROUGH</th>
+                    <th>SENT THROUGH <i class="fa fa-info-circle" aria-hidden="true"></i></th>
                     <th>DATE CREATED</th>
                     <th>START DATE</th>
                     <th>END DATE</th>
@@ -45,79 +45,79 @@ function Table() {
                     <td><i class="fa fa-bookmark" aria-hidden="true"></i><i class="fa fa-mobile" aria-hidden="true"></i></td>
                     <td>08/30/2020</td>
                     <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td>09/30/2020<p className="time-end">6:00 PM</p></td>
                 </tr>
                 <tr>
                     <td>Mid-Autumn Festi...</td>
                     <td>We wish everyone a...</td>
                     <td>Doe, Jane</td>
-                    <td><i class="fa fa-bookmark" aria-hidden="true"></i></td>
-                    <td>08/30/2020</td>
-                    <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td><i class="fa fa-bookmark" aria-hidden="true"></i><i class="fa fa-mobile" aria-hidden="true"></i></td>
+                    <td>08/22/2020</td>
+                    <td>08/23/2020<p className="time-start">12:00 PM</p></td>
+                    <td>08/25/2020<p className="time-end">9:30 AM</p></td>
                 </tr>
                 <tr>
                     <td>50th General Asse...</td>
                     <td>Expelliarmus! Chec...</td>
                     <td>Doe, Jane</td>
-                    <td><i class="fa fa-bookmark" aria-hidden="true"></i></td>
-                    <td>08/30/2020</td>
-                    <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td><i class="fa fa-bookmark" aria-hidden="true"></i><i class="fa fa-mobile" aria-hidden="true"></i></td>
+                    <td>08/15/2020</td>
+                    <td>08/23/2020<p className="time-start">9:00 AM</p></td>
+                    <td>08/25/2020<p className="time-end">6:30 PM</p></td>
                 </tr>
                 <tr>
                     <td>New Employees</td>
                     <td>Let us all welcom...</td>
                     <td>King, Chow</td>
                     <td><i class="fa fa-bookmark" aria-hidden="true"></i></td>
-                    <td>08/30/2020</td>
-                    <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td>08/10/2020</td>
+                    <td>08/15/2020<p className="time-start">8:30 AM</p></td>
+                    <td>08/18/2020<p className="time-end">6:30 PM</p></td>
                 </tr>
                 <tr>
                     <td>Stocks as of Aug...</td>
                     <td>Our investors have...</td>
                     <td>King, Chow</td>
                     <td><i class="fa fa-bookmark" aria-hidden="true"></i></td>
-                    <td>08/30/2020</td>
-                    <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td>07/30/2020</td>
+                    <td>08/01/2020<p className="time-start">9:00 AM</p></td>
+                    <td>08/04/2020<p className="time-end">6:30 PM</p></td>
                 </tr>
                 <tr>
                     <td>Tip of the Week</td>
                     <td>To stay hydrated, d...</td>
                     <td>Roe, Ken</td>
                     <td><i class="fa fa-bookmark" aria-hidden="true"></i></td>
-                    <td>08/30/2020</td>
-                    <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td>02/26/2020</td>
+                    <td>02/27/2020<p className="time-start">9:00 AM</p></td>
+                    <td>03/15/2020<p className="time-end">6:30 PM</p></td>
                 </tr>
                 <tr>
                     <td>Employees of the M...</td>
                     <td>Congratulations to ...</td>
                     <td>King, Chow</td>
-                    <td><i class="fa fa-bookmark" aria-hidden="true"></i></td>
-                    <td>08/30/2020</td>
-                    <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td><i class="fa fa-mobile" aria-hidden="true"></i></td>
+                    <td>01/12/2020</td>
+                    <td>01/13/2020<p className="time-start">9:00 AM</p></td>
+                    <td>12/20/2019<p className="time-end">6:30 PM</p></td>
                 </tr>
                 <tr>
                     <td>Breakfast Menu</td>
                     <td>We will be introduci...</td>
                     <td>Gus, Amon</td>
-                    <td><i class="fa fa-bookmark" aria-hidden="true"></i></td>
-                    <td>08/30/2020</td>
-                    <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td><i class="fa fa-mobile" aria-hidden="true"></i></td>
+                    <td>12/15/2019</td>
+                    <td>12/20/2019<p className="time-start">9:00 AM</p></td>
+                    <td>12/20/2019<p className="time-end">6:30 PM</p></td>
                 </tr>
                 <tr>
                     <td>Cafe Relocation</td>
                     <td>With the ongoing re...</td>
                     <td>Lavjsta, Asta</td>
-                    <td><i class="fa fa-bookmark" aria-hidden="true"></i></td>
-                    <td>08/30/2020</td>
-                    <td>09/01/2020<p className="time-start">9:00 AM</p></td>
-                    <td>09/30/2020<p className="time-end">6:30 PM</p></td>
+                    <td><i class="fa fa-bookmark" aria-hidden="true"></i><i class="fa fa-mobile" aria-hidden="true"></i></td>
+                    <td>12/14/2019</td>
+                    <td>12/15/2019<p className="time-start">9:00 AM</p></td>
+                    <td>12/19/2019<p className="time-end">6:30 PM</p></td>
                 </tr>
                 <tfoot>
                     <tr>
@@ -129,7 +129,7 @@ function Table() {
                         <td className="center" colspan="5">
                             <i class="fa fa-step-backward" aria-hidden="true"></i>
                             <i class="fa fa-caret-left" aria-hidden="true"></i>
-                            Page<span>1</span> of 1 <i class="fa fa-caret-right" aria-hidden="true"></i>
+                            Page<div className="page-num">1</div> of 1 <i class="fa fa-caret-right" aria-hidden="true"></i>
                             <i class="fa fa-step-forward" aria-hidden="true"></i>
                         </td>
                         <td className="right">Showing 1 - 10 of 1</td>
